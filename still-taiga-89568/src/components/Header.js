19da514e9528dd62430cc1432e0d9875765e2474
Eric Vo <vo.eric@gmail.com>
2018-11-25 21:32:21 -0500

    add placeholder text and styling to header

--- still-taiga-89568/src/components/Header.js
+++ still-taiga-89568/src/components/Header.js
@@ -1,25 +1,21 @@
-import React, {Component} from 'react'
-import './Header.css'
+import React, { Component } from 'react';
+import './Header.css';
 
 class Header extends Component {
-    render() {
-        
-      return (
-        /*
-        Headings
-        */
-        <div>
-          <div className = 'mainHeader'>
-            I eat too much when I am sad
-            </div>
-          <div className = 'subHeader'>
-            Wednesday, Nov 18, 2018
-          </div>
-          <div>  </div>
+  render() {
+    return (
+      <div>
+        <p className="mainheader">I eat too much when I am sad</p>
+        <p className="subheader">Wednesday, Nov 18, 2018</p>
+        <div className="header-session">
+          <p>10 people currently in this session</p>
+          <p>
+            Invite Participants <span className="plus-button">+</span>
+          </p>
         </div>
-      )
-    }
-
+      </div>
+    );
+  }
 }
 
 export default Header;
